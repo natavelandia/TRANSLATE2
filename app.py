a93eed8341d28c91d1783b41cfd3de3e924c96f3
natavelandia <39436355+natavelandia@users.noreply.github.com>
2023-06-01 13:55:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,16 @@ global translator_endpoint
 global cog_key    
 global cog_region
 
-try:
-    cog_key = os.environ.get("COG_SERVICE_KEY")
-    cog_region = os.environ.get("COG_SERVICE_REGION")      
-    translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
-except Exception as ex:        
-    print(ex)
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
+from flask import Flask,render_template
+app= Flask (__name__)
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        text = request.form['text']
-        # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
-        source_language = ''
-        translated_text = text
+    return render_template('index.html')
 
-        return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
+@app.route('/about.html')
+def about():
+    pass
     
-    return render_template('home.html')
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
